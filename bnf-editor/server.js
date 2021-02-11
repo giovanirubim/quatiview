7b67026c25e2f62d7382faea50662484712e0196
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-02-11 12:37:02 -0300

    Editor de BNF

---
+++ bnf-editor/server.js
@@ -0,0 +1,46 @@
+let Path = require('path')
+let http = require('http')
+let fs = require('fs')
+let filePath = Path.join(__dirname, 'bnf.txt')
+let port = 9137
+const mimeMap = {
+	'html': 'text/html',
+	'js': 'application/javascript',
+	'txt': 'text/plain',
+}
+const getMime = (path) => {
+	return mimeMap[path.replace(/^.*\.(\w+)$/, '$1')] || 'application/octet-stream'
+}
+http.createServer((req, res) => {
+	let {url, method} = req
+	let path = Path.join(__dirname, url.replace(/[?#].*/, '').replace(/\/$/, '/index.html'))
+	if (method === 'GET') {
+		try {
+			const buffer = fs.readFileSync(path)
+			res.writeHead(200, {
+				'content-type': getMime(path),
+				'content-length': buffer.length
+			})
+			res.end(buffer)
+		} catch(err) {
+			res.writeHead(404)
+			res.end()
+		}
+		return
+	}
+	if (method !== 'POST') {
+		res.writeHead(404)
+		res.end()
+	}
+	const chunks = []
+	req.on('data', chunk => chunks.push(chunk))
+	req.on('end', () => {
+		console.log('stored')
+		const str = Buffer.concat(chunks).toString('utf8')
+		fs.writeFileSync(filePath, str)
+		res.writeHead(200)
+		res.end()
+	})
+}).listen(port, () => {
+	console.log(`http://localhost:${port}/`)
+})
