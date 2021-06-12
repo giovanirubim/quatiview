446eee465afa37579828a3c04ba56fde3bef33e0
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-06-11 23:06:56 -0300

    Adds errors: CompilationError, SyntaitError

--- modules/Errors.mjs
+++ modules/Errors.mjs
@@ -1,6 +1,19 @@
-export class LexycalError extends Error {
+export class CompilationError extends Error {
+	constructor(message) {
+		super(message);
+	}
+}
+
+export class LexycalError extends CompilationError {
 	constructor(index) {
 		super('Lexycal error at position ' + index);
+		this.index = index;
+	}
+}
+
+export class SyntaticError extends CompilationError {
+	constructor(index) {
+		super('Syntax error at position' + index);
 		this.index = index;
 	}
 }
