fe83a2b78b3d2d906c6ab835e37d3adb40a92554
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-08-18 21:54:58 -0300

    Using one exports for each non terminal instead of a string list

--- parser-module/src/PatternDefinitions/NonTerminals.js
+++ parser-module/src/PatternDefinitions/NonTerminals.js
@@ -1,12 +1,1 @@
-const list = `
-
-	CONSTANT
-
-`;
-
-module.exports = Object.fromEntries(
-	list
-		.trim()
-		.split(/\s*\n\s*/)
-		.map((name) => [ name, `NT-${name}` ]),
-);
+module.exports.CONSTANT = 'CONSTANT';
