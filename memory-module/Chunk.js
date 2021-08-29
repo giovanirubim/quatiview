67fafdbba1f3ec94a2ec9d008b31ad5881ac0c4c
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-08-29 00:00:32 -0300

    Chunk class

---
+++ memory-module/Chunk.js
@@ -0,0 +1,73 @@
+class Chunk {
+	constructor({
+		prev = null,
+		next = null,
+		allocated = false,
+		address,
+		size,
+	}) {
+		this.prev = prev;
+		this.next = next;
+		this.allocated = allocated;
+		this.address = address;
+		this.size = size;
+	}
+	allocate({ address, size }) {
+		const end = this.address + this.size;
+		const blockEnd = address + size;
+		if (blockEnd < end) {
+			const size = end - blockEnd;
+			this.split({ size, allocated: false });
+		}
+		let chunk = this;
+		if (address > this.address) {
+			this.split({ size, allocated: true });
+		} else {
+			this.allocated = true;
+		}
+	}
+	free() {
+		this.allocated = false;
+		if (this.next?.allocated === false) {
+			this.merge();
+		}
+		if (this.prev?.allocated === false) {
+			this.prev.merge();
+		}
+		return this;
+	}
+	split({ size, allocated }) {
+		const end = this.address + this.size;
+		const address = end - size;
+		const chunk = new Chunk({
+			prev: this,
+			next: this.next,
+			allocated,
+			address,
+			size,
+		});
+		this.next = chunk;
+		this.size = address - this.address;
+		return chunk;
+	}
+	merge() {
+		const chunk = this.next;
+		this.size += chunk.size;
+		this.next = chunk.next;
+		if (this.next) {
+			this.next.prev = this;
+		}
+		return this;
+	}
+	toArray() {
+		const array = [{
+			allocated: this.allocated,
+			address: this.address,
+			size: this.size,
+		}];
+		if (this.next) {
+			array.push(...this.next.toArray());
+		}
+		return array;
+	}
+}
