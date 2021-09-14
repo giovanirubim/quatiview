fdb4d34c9125c45d10c9bb0832b4dd01d2219ed0
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-14 20:01:19 -0300

    Fix: parseOp1

--- web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/parseOp1.js
+++ web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/parseOp1.js
@@ -1,7 +1,12 @@
 import ParseTreeNode from '../ParseTreeNode.js';
-import parseOp0 from '../parseOp0.js';
-import zeroToMany from '../zeroToMany.js';
-import oneOf from '../oneOf.js';
+
+import zeroToMany from './zeroToMany.js';
+import oneOf from './oneOf.js';
+
+import parseArgCall from './parseArgCall.js'
+import parseIndexAcc from './parseIndexAcc.js'
+import parseMemberAcc from './parseMemberAcc.js'
+import parseOp0 from './parseOp0.js';
 
 const parseOperation = (tokenGenerator) => oneOf(
     tokenGenerator,
@@ -14,7 +19,7 @@ export default (tokenGenerator) => {
     let item, opertaions;
     const children = [
         item = parseOp0(tokenGenerator),
-        opertaions = zeroToMany(tokenGenerator, parseOperation),
+        ... (opertaions = zeroToMany(tokenGenerator, parseOperation)),
     ];
 	return new ParseTreeNode({
 		typeName: 'op1',
