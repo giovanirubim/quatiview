281d8b5ccfc47661cf8fcf17c6b125507a8776d7
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-14 17:19:30 -0300

    Method attempt

---
+++ web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/attempt.js
@@ -0,0 +1,15 @@
+import { SyntaticError } from "../../../../errors.js";
+
+export default (context, parser) => {
+    const state = context.getState();
+    try {
+        return parser(context);
+    } catch(error) {
+        if (error instanceof SyntaticError) {
+            context.setState(state);
+            return null;
+        } else {
+            throw error;
+        }
+    }
+};
