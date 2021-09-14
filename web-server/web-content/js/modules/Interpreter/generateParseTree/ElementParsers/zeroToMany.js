05f2fcc97229b69d996338b236e8f3df67cad1b6
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-14 01:44:09 -0300

    oneOf and zeroToMany

---
+++ web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/zeroToMany.js
@@ -0,0 +1,17 @@
+import { SyntaticError } from "../../../../errors.js";
+
+export default (context, parser) => {
+    const array = [];
+    for (;;) {
+        const state = context.getState();
+        try {
+            array.push(parser(context));
+        } catch(error) {
+            if (error instanceof SyntaticError) {
+                context.setState(state);
+                return array;
+            }
+            throw error;
+        }
+    }
+};
