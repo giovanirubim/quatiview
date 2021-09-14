685386f2c240509bdc44d8e24ad41f6a675828de
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-14 18:09:52 -0300

    Template parser simplified

--- web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/template.js
+++ web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/template.js
@@ -1,6 +1,4 @@
 import ParseTreeNode from '../ParseTreeNode.js';
-import zeroToMany from './zeroToMany.js';
-import oneOf from './oneOf.js';
 
 export default (tokenGenerator) => {
 	return new ParseTreeNode({
