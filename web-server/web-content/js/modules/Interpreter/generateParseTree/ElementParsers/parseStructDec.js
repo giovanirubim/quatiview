554fa07fbc805c65e5ab3d9de59376d0f4c0d55c
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-14 16:02:06 -0300

    Parser: struct_dec

---
+++ web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/parseStructDec.js
@@ -0,0 +1,24 @@
+import ParseTreeNode from '../ParseTreeNode.js';
+import oneToMany from './oneToMany.js';
+
+import parseStructType from './parseStructType.js';
+import parseVarDec from './parseVarDec.js';
+
+export default (tokenGenerator) => {
+    const type = parseStructType(tokenGenerator);
+    const children = [
+        type,
+        tokenGenerator.pop('left-brackets'),
+    ];
+    const vars = oneToMany(tokenGenerator, parseVarDec);
+    children.push(tokenGenerator.pop('right-brackets'));
+    children.push(tokenGenerator.pop('semicolon'));
+	return new ParseTreeNode({
+		typeName: 'struct-dec',
+        content: {
+            type,
+            vars,
+        },
+		children,
+	});
+};
