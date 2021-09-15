2405dc63983a375f4ac02ae0227ae487a3b8f6ec
giovanirubim <giovanirubim.f@gmail.com>
2021-09-15 00:03:23 -0300

    Add null to parseConst

--- web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/parseConst.js
+++ web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/parseConst.js
@@ -1,7 +1,12 @@
 import ParseTreeNode from '../ParseTreeNode.js';
 
 export default (tokenGenerator) => {
-	const content = tokenGenerator.pop('int-const', 'char-const', 'str-const');
+	const content = tokenGenerator.pop(
+		'int-const',
+		'char-const',
+		'str-const',
+		'NULL',
+	);
 	return new ParseTreeNode({
 		typeName: 'const',
 		children: [ content ],
