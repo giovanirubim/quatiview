719161c9537796a6140fae7343f220dd27a35d74
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-14 22:25:41 -0300

    Template parser removed

--- web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/template.js
+++
@@ -1,9 +0,0 @@
-import ParseTreeNode from '../ParseTreeNode.js';
-
-export default (tokenGenerator) => {
-	return new ParseTreeNode({
-		typeName: '',
-        content: null,
-		children: [],
-	});
-};
