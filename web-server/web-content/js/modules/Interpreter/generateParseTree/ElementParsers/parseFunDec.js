f6646f3ff4f2b547bb1818df0f5da5ecceb1c75d
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-14 22:00:43 -0300

    parseFunDec

---
+++ web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/parseFunDec.js
@@ -0,0 +1,30 @@
+import ParseTreeNode from '../ParseTreeNode.js';
+
+import attempt from './attempt.js';
+
+import parseArgList from './parseArgList.js';
+import parseType from './parseType.js';
+import parseScope from './parseScope.js';
+
+export default (tokenGenerator) => {
+    let type, asterisks, id, argList, scope;
+    const children = [
+        type = parseType(tokenGenerator),
+        ... (asterisks = tokenGenerator.popMany('asterisk')),
+        id = tokenGenerator.pop('id'),
+        tokenGenerator.pop('left-parentheses'),
+        argList = attempt(tokenGenerator, parseArgList),
+        tokenGenerator.pop('right-parentheses'),
+        scope = parseScope(tokenGenerator),
+    ];
+	return new ParseTreeNode({
+		typeName: 'fun-dec',
+        content: {
+            type,
+            name: id.content,
+            argList,
+            scope,
+        },
+		children: children.filter((item) => item !== null),
+	});
+};
