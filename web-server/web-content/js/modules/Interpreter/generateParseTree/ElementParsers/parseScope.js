79de444d20b535439c9f11f23eb754c61937a74c
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-14 22:25:04 -0300

    parseScope Fixed

--- web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/parseScope.js
+++ web-server/web-content/js/modules/Interpreter/generateParseTree/ElementParsers/parseScope.js
@@ -3,12 +3,15 @@ import parseLocalLine from './parseLocalLine.js';
 import zeroToMany from './zeroToMany.js';
 
 export default (tokenGenerator) => {
-    let lines;
-    const children = [
-        tokenGenerator.pop('left-brackets'),
-        ... (lines = zeroToMany(tokenGenerator, parseLocalLine)),
-        tokenGenerator.pop('right-brackets'),
-    ];
+    const children = [];
+    const lines = [];
+    children.push(tokenGenerator.pop('left-brackets'));
+    while (!tokenGenerator.nextIs('right-brackets')) {
+        const line = parseLocalLine(tokenGenerator);
+        lines.push(line);
+        children.push(line);
+    }
+    children.push(tokenGenerator.pop('right-brackets'));
 	return new ParseTreeNode({
 		typeName: 'scope',
         content: lines,
