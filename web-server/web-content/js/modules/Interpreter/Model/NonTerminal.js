b461d9f0c30721cafaa567a94a19f9cfff44755c
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-16 20:28:15 -0300

    WIP

--- web-server/web-content/js/modules/Interpreter/Model/NonTerminal.js
+++ web-server/web-content/js/modules/Interpreter/Model/NonTerminal.js
@@ -2,21 +2,21 @@ import ParseTreeNode from './ParseTreeNode.js';
 
 let map = [];
 
-export default class TreeParser {
+export default class NonTerminal {
     constructor({ name, parse }) {
         this.name = name;
         this.parse = parse;
         map[name] = this;
     }
     parse(name, tokenGenerator) {
-        const treeParser = map[name];
-        if (treeParser === undefined) {
-            throw `No TreeParser defined for ${name}`;
+        const nonTerminal = map[name];
+        if (nonTerminal === undefined) {
+            throw `No NonTerminal defined for ${name}`;
         }
         const prevTarget = tokenGenerator.target;
         const children = [];
         tokenGenerator.target = children;
-        const args = treeParser.parse(tokenGenerator);
+        const args = nonTerminal.parse(tokenGenerator);
         tokenGenerator.target = prevTarget;
         return new ParseTreeNode({ ... args, children });
     }
