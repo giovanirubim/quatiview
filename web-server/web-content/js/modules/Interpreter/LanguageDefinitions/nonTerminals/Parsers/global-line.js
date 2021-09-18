b3fe0dd9bdb1d1ceb65211aa8762599471ead5be
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-18 00:32:03 -0300

    WIP

--- web-server/web-content/js/modules/Interpreter/LanguageDefinitions/nonTerminals/Parsers/global-line.js
+++ web-server/web-content/js/modules/Interpreter/LanguageDefinitions/nonTerminals/Parsers/global-line.js
@@ -2,5 +2,5 @@ import NonTerminal from '../../../Model/NonTerminal.js';
 
 new NonTerminal({
     name: 'global-line',
-    parse: (ctx) => ctx.parseOneOf('func-dec', 'var-dec', 'struct-dec'),
+    parse: (ctx) => ctx.parseOneOf('fun-dec', 'var-dec', 'struct-dec'),
 });
