dd05327c309b02e96f10f63d07b65fac59eb654c
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-09-30 00:02:00 -0300

    WIP

--- web-server/web-content/js/main.js
+++ web-server/web-content/js/main.js
@@ -9,73 +9,46 @@ $(document).ready(() => {
 	Net.terminal.init();
 	Net.memViewer.init();
 
+	Net.memViewer.addStruct('linked_list')
+	.member({
+		offset: 0, type: 'int',
+		col: 0, row: 0, length: 3,
+	})
+	.member({
+		offset: 4, type: 'self*',
+		col: 0, row: 1, length: 3,
+	});
+
 	Net.memViewer.addStruct('binary_search_tree')
 	.member({
-		offset: 0,
-		type: 'int',
-		col: 0,
-		row: 0,
-		length: 4,
+		offset: 0, type: 'int',
+		col: 0, row: 0, length: 4,
 	})
 	.member({
-		offset: 4,
-		type: 'self*',
-		col: 0,
-		row: 1,
-		length: 2,
+		offset: 4, type: 'self*',
+		col: 0, row: 1, length: 2,
 	})
 	.member({
-		offset: 8,
-		type: 'self*',
-		col: 2,
-		row: 1,
-		length: 2,
+		offset: 8, type: 'self*',
+		col: 2, row: 1, length: 2,
 	});
 
 	Net.memViewer.addStruct('avl')
 	.member({
-		offset: 0,
-		type: 'int',
-		col: 0,
-		row: 0,
-		length: 3,
+		offset: 0, type: 'int',
+		col: 0, row: 0, length: 3,
 	})
 	.member({
-		offset: 4,
-		type: 'int',
-		col: 3,
-		row: 0,
-		length: 1,
+		offset: 4, type: 'int',
+		col: 3, row: 0, length: 1,
 	})
 	.member({
-		offset: 8,
-		type: 'self*',
-		col: 0,
-		row: 1,
-		length: 2,
+		offset: 8, type: 'self*',
+		col: 0, row: 1, length: 2,
 	})
 	.member({
-		offset: 12,
-		type: 'self*',
-		col: 2,
-		row: 1,
-		length: 2,
-	});
-
-	Net.memViewer.addStruct('linked_list')
-	.member({
-		offset: 0,
-		type: 'int',
-		col: 0,
-		row: 0,
-		length: 3,
-	})
-	.member({
-		offset: 4,
-		type: 'self*',
-		col: 0,
-		row: 1,
-		length: 3,
+		offset: 12, type: 'self*',
+		col: 2, row: 1, length: 2,
 	});
 
 });
