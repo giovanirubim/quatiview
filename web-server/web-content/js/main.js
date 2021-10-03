6ddb87945369fddd7c004163f1cb1ae89c2c684a
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-10-03 19:37:05 -0300

    AVL added

--- web-server/web-content/js/main.js
+++ web-server/web-content/js/main.js
@@ -33,4 +33,22 @@ $(document).ready(() => {
 		col: 2, row: 1, length: 2,
 	});
 
+	Net.memViewer.addStruct('avl_tree')
+	.member({
+		offset: 0, type: 'int',
+		col: 0, row: 0, length: 3,
+	})
+	.member({
+		offset: 4, type: 'int',
+		col: 3, row: 0, length: 1,
+	})
+	.member({
+		offset: 8, type: 'self*',
+		col: 0, row: 1, length: 2,
+	})
+	.member({
+		offset: 12, type: 'self*',
+		col: 2, row: 1, length: 2,
+	});
+
 });
