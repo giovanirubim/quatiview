0dd192fbda71ecd1d6837707bab750def8b229c5
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2021-10-03 19:30:29 -0300

    Web-server ENV port

--- web-server/index.js
+++ web-server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webDir = path.join(__dirname, '/web-content/');
 const express = require('express');
 const app = express();
-const port = process.env.PORT ?? 80;
+const port = process.env.PORT || 8080;
 app.use(express.static(webDir))
 app.use((req, res, next) => {
 	console.log(`Not found: ${req.url}`);
